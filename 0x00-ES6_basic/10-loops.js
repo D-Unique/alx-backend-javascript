2b6c3458fd7039af9e580aae40819dc10fb127a0
Unigwe <emmunigwe@gmail.com>
2024-10-03 20:26:28 +0100

    feat(getResponseFromAPI): This function implements the use of promise

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
-  for (const value of array) {
-    newArray.push(appendString + value);
+  for (let idx of array) {
+    array.appendToEachArrayValue(appendString);
   }
-  return newArray;
+
+  return array;
 }
