2e5b09601ec74095d2aa90040c59e912f53a9c0f
Unigwe <emmunigwe@gmail.com>
2024-10-03 20:35:03 +0100

    feat(getResponseFromAPI): This function implements the use of promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+export function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const response = {
+        data: 'Success',
+        status: '200',
+      };
+      resolve(response);
+    }, 2000);
+});
+}
+
+export default getResponseFromAPI;
