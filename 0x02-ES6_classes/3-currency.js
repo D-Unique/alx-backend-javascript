0cb102f8e9a7d369a0ee7e393a3b03a283b7629d
Unigwe <emmunigwe@gmail.com>
2024-10-04 16:34:08 +0100

    feat(3-currency.js): Added a new function

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+    if (typeof code !== 'string') throw new TypeError('Code should be 0f type string');
+    if (typeof name !== 'string') throw new TypeError('name should be 0f type string');
+  }
+
+  set code(val) {
+    if (typeof (val) !== 'String') throw new TypeError(' Code should be 0f type string');
+    this._val = val;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(val) {
+    if (typeof (val) !== 'String') throw new TypeError(' name should be 0f type string');
+    this._val = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
