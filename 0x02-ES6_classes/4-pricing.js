2d68aea17e15580a8431544c942c3ee0c14dd4da
Unigwe <emmunigwe@gmail.com>
2024-10-04 22:57:42 +0100

    feat(4-pricing.js): added a new function

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing extends Currency {
+  Constructor(amount, Currency) {
+    this._amount = amount;
+    this._Currency = Currency;
+    if (!Number.isInteger(amount)) throw new TypeError('amount must be a number');
+    if (!Number.isInteger(Currency)) throw new TypeError('Currency must be a number');
+  }
+
+  set amount(val) {
+    if (!Number.isInteger(val)) throw new TypeError(`${val} must be a number`);
+    this._val = val;
+  }
+
+  get amount() {
+    return this._val;
+  }
+
+  set Currency(val) {
+    if (!Number.isInteger(val)) throw new TypeError(`${val} must be a number`);
+    this._val = val;
+  }
+
+  get Currency() {
+    return this._val;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._Currency._name} ${(this._Currency._code)}.`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
