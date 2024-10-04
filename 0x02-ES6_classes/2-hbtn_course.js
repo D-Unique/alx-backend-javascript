f7a3c9e5c03ff1f43f73c15b0d3663dc27b88325
Unigwe <emmunigwe@gmail.com>
2024-10-04 16:05:51 +0100

    feat(2-hbtn_course.js ): Add a class HolbertonCourse with the setter and getter prop

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,40 +1,38 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (!Number.isInteger(length)) throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
+
     this._name = name;
     this._length = length;
     this._students = students;
-  }
-
-  set name(name) {
-    if (typeof (name) === String) {
-      return this._name = name;
-    }
-    console.error(`${name} must be a string`);
-  }
-
-  set length(length) {
-    if (typeof (length) === Number) {
-      return this._length = length;
-    }
-    console.error(`${length} must be a number`);
-  }
-
-  set students(students) {
-    if (typeof (students) === Array) {
-      return this._students = students;
-    }
-    console.error(`${students} must be a number`);
   }
 
   get name() {
     return this._name;
   }
 
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('name must be a string');
+    this._name = val;
+  }
+
   get length() {
     return this._length;
   }
 
+  set length(val) {
+    if (!Number.isInteger(val)) throw new TypeError('length must be a number');
+    this._length = val;
+  }
+
   get students() {
     return this._students;
+  }
+
+  set students(val) {
+    if (!Array.isArray(val)) throw new TypeError('students type must be an Array');
+    this._students = val;
   }
 }
