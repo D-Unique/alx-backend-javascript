9c474434de5ecba1163b355c1ac99b883912ebc5
Unigwe <emmunigwe@gmail.com>
2024-10-05 13:22:47 +0100

    fixed

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -1,4 +1,4 @@
-import handleResponseFromAPI from "./2-then";
+import getFullResponseFromAPI from './1-promise';
 
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
-export function onsecess() {
-  return { status: '200', body: 'success' };
-}
-
-export function onreject() {
-  return new Error();
-}
-
 export default function handleResponseFromAPI(promise) {
-  return promise.then(onsecess(), onreject()).finally(console.log('Got a response from the API to the console'));
+  return promise
+    .then(() => ({ status: '200', body: 'Success' }))
+    .catch((error) => console.error(error.message))
+    .finally(console.log('Got a response from the API to the console'));
 }
