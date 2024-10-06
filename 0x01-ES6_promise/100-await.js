d94e23d86f519ad9fd844e062332dc9ce18905ef
Unigwe <emmunigwe@gmail.com>
2024-10-06 04:19:32 +0100

    fixed(100-await.js): handled error

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const [photo1, user1] = await Promise.all([uploadPhoto(), createUser()]);
+  const [photo1, user1] = await Promise.all([uploadPhoto(), createUser()]).catch((err) => {
+    console.error(err)});
   const p1 = {
     photo: { status: photo1.status, body: photo1.body },
     user: { firstName: user1.firstName, lastName: user1.lastName },
