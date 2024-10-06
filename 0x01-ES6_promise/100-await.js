f63911593c7b359f27f42ef7c7d153e3c544d23c
Unigwe <emmunigwe@gmail.com>
2024-10-06 04:14:49 +0100

    fixed(100-await.js): display format

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-
-  return Promise.all([photo, user]).then((value) => value);
+  const [photo1, user1] = await Promise.all([uploadPhoto(), createUser()]);
+  const p1 = {
+    photo: { status: photo1.status, body: photo1.body },
+    user: { firstName: user1.firstName, lastName: user1.lastName },
+  };
+  return p1;
 }
