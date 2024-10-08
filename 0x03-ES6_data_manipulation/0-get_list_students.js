c7d2af1e6ad8a460137ba3f8db298557e11194aa
Unigwe <emmunigwe@gmail.com>
2024-10-08 21:49:08 +0100

    feat(0-get_list_students.js): add new func, readme.md

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+export default function getListStudents() {
+  const map1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const map2 = { id: 2, firstName: 'James', location: 'Columbia' };
+
+  const map3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  const arrObj = [map1, map2, map3];
+
+  return arrObj;
+}
