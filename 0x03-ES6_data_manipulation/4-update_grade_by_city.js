2381391fe3431754a5fc47c20fc959a8f935637e
Unigwe <emmunigwe@gmail.com>
2024-10-11 11:10:29 +0100

    fix(4-update_grade_by_city.js): eslint iss

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,15 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
-    const flist = list.filter((list) => list.location === city)
-    return flist.map((flist) =>  {
-        newGrades.map((sGrade) => {
-            if (sGrade.studentId === flist.id) {
-                flist.grade = sGrade.grade
-            } 
-            if (!flist.hasOwnProperty('grade')) {
-                flist.grade = 'N/A';
-            }
-            return flist
-
-        })
-        return flist
-    })
-
+  const flist = list.filter((list) => list.location === city);
+  return flist.map((flist) => {
+    newGrades.map((sGrade) => {
+      if (sGrade.studentId === flist.id) {
+        flist.grade = sGrade.grade;
+      }
+      if (!flist.hasOwnProperty('grade')) {
+        flist.grade = 'N/A';
+      }
+      return flist;
+    });
+    return flist;
+  });
 }
