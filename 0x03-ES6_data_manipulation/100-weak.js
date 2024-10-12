36f88383cf63dffdc24db6e015eee126bef4f780
Unigwe <emmunigwe@gmail.com>
2024-10-12 02:40:27 +0100

    fixed( 100-weak.js)

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,7 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  let value = (weakMap.get(endpoint) || 0);
+  const value = (weakMap.get(endpoint) || 0) + 1;
   if (value >= 5) throw new Error('Endpoint load is high');
-  value += 1;
   weakMap.set(endpoint, value);
 }
