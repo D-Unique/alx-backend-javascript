8d4abc4765ee4847f0a8eec58ceac62e332f786d
Unigwe <emmunigwe@gmail.com>
2024-11-27 01:26:08 +0100

    feat1: 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,25 +3,21 @@ const fs = require('node:fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf-8');
+    const data = fs.readFileSync(path, 'utf8');
     const result = papa.parse(data, {
-      headers: true,
+      header: true,
       skipEmptyLines: true,
     }).data;
+
     console.log(`Number of students: ${result.length}`);
     const numberOfCsStudentList = [];
     const numberOfSweStudentList = [];
-    result.forEach((element) => {
-      if (element[0] === 'firstname') {
-        return numberOfCsStudentList;
-      }
-
-      return element[element.length - 1] === 'CS' ? numberOfCsStudentList.push(element[0]) : numberOfSweStudentList.push(element[0]);
-    });
-    console.log(`Number of students in CS: ${numberOfCsStudentList.length}. List: ${numberOfCsStudentList} `);
-    console.log(`Number of students in SWE: ${numberOfSweStudentList.length}. List: ${numberOfSweStudentList} `);
+    result.forEach((element) => ((element.field === 'CS') ? numberOfCsStudentList.push(element.firstname) : numberOfSweStudentList.push(element.firstname)));
+    console.log(`Number of students in CS: ${numberOfCsStudentList.length}. List: ${numberOfCsStudentList.join(', ')} `);
+    console.log(`Number of students in SWE: ${numberOfSweStudentList.length}. List: ${numberOfSweStudentList.join(', ')} `);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
