b52bab2d92d9a1fc56466ba2019bbf27b8734a55
Unigwe <emmunigwe@gmail.com>
2024-11-28 18:55:14 +0100

    test task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,33 +2,26 @@ const fs = require('fs');
 const Papa = require('papaparse');
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const parsedData = Papa.parse(data, {
-      header: true,
-      skipEmptyLines: true,
-
-    }).data;
-
-    const fields = {};
-    parsedData.forEach((student) => {
-      const { field } = student;
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(student.firstname);
-    });
-
-    console.log(`Number of students: ${parsedData.length}`);
-
-    for (const field in fields) {
-      if (field in fields) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    } else {
+      const parsedData = Papa.parse(data, { header: true, skipEmptyLines: true }).data;
+      const fields = {};
+      parsedData.forEach((student) => {
+        const { field } = student;
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(student.firstname);
+      });
+      console.log(`Number of students: ${parsedData.length}`);
+      for (const field in fields) {
+        if (field in fields) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
       }
     }
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+  });
 }
-
 module.exports = countStudents;
