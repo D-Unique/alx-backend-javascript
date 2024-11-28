2b960954878193fdd101dd19b1870a83e93d310e
Unigwe <emmunigwe@gmail.com>
2024-11-28 19:22:08 +0100

    test 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,30 @@
-const fs = require('fs')
-const papa = require('papaparse')
+const fspromises = require('fs').promises;
+const papa = require('papaparse');
 
-function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf-8')
-        const parsedData = papa.parse(data, {
-            header: true,
-            skipEmptyLines: true
-        }).data
-        const fields = {}
-        parsedData.forEach((student) => {
-            const { field } = student
-            if (!fields[field]) {
-                fields[field] = []
-            }
-            fields[field].push(student.firstname)
-        })
-        console.log(`Number of students: ${parsedData.length}`)
-        for (const field in fields) {
-            if (field in fields) {
-                console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`)
-            }
-        }
+async function countStudents(path) {
+  try {
+    const data = await fspromises.readFile(path, 'utf-8');
+    const parsedData = papa.parse(data, {
+      header: true,
+      skipEmptyLines: true,
+    }).data;
+    const fields = {};
+    parsedData.forEach((student) => {
+      const { field } = student;
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student.firstname);
+    });
+    console.log(`Number of students: ${parsedData.length}`);
+    for (const field in fields) {
+      if (field in fields) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
     }
-    catch (err) {
-        throw new Error('Cannot load the database')
-    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
-module.exports = countStudents
+module.exports = countStudents;
