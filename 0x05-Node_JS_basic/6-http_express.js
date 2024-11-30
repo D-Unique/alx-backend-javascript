4698bc75a1331f5035aba407049691b06d4ac9cd
Unigwe <emmunigwe@gmail.com>
2024-11-30 18:22:36 +0100

    feat 6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const port = 1245;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`the sserver is running on http//:localhost:${port}`);
+});
+
+module.exports = app;
