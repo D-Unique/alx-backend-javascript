d9fbbd6fbc8e88345348429f4f0b509d571a9c8a
Unigwe <emmunigwe@gmail.com>
2024-12-01 20:37:19 +0100

    fixing (0-calcul.test.js)

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,5 +4,7 @@ const calculateNumber = require('./0-calcul');
 
 it('it should be equal 3', function () {
     assert.equal(calculateNumber(1.2, 1.5), 3);
-
 });
+it(' it should equal 4', function () {
+    assert.equal(calculateNumber(2.6, 1.1), 4);
+})
