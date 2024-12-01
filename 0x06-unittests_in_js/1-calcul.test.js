579a6b29134dddb5c51934c87909aeda73a913a0
Unigwe <emmunigwe@gmail.com>
2024-12-01 23:13:24 +0100

    feat(1-calcul.test.js)

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,16 @@
+const calculateNumber = require('./1-calcul')
+const assert = require('assert')
+
+describe('operation', function () {
+    it('SUM', function () {
+        assert.equal(calculateNumber('SUM', 3.3, 3.7), 7)
+    })
+    it('SUBTRACT', function () {
+        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
+    })
+    it('DIVIDE', function () {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error')
+    })
+
+})
+
